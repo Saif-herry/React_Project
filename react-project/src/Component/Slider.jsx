b650aa293e11b30a74a9ddb55baf5502f9d1e174
Mohammad Saif <saifsami321md@gmail.com>
2023-03-11 23:58:38 +0530

    update

---
+++ react-project/src/Component/Slider.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react'
+import { useState } from 'react';
+
+
+const Slider = () => {
+    const [index, setIndex] = React.useState(0);
+    const tRef = useRef(0);
+
+    
+ useEffect(() => {
+
+}, [])
+
+
+
+  return (
+<div>
+
+
+</div>
+    )
+}
+
+export default Slider
--- react-project/src/Routes/MainRoutes.jsx
+++ react-project/src/Routes/MainRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Apod from '../Pages/Apod'
+import Slider from '../Component/Slider'
 
 const MainRoutes = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Apod/>} />
+        <Route path='/slider' element={<Slider/>}/>
       </Routes>
     </>
   )
